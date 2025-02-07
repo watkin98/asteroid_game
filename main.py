2c23de7501e75c86faa8cdbcd21a7083e35d8df9
watkin98 <wvatkin1@gmail.com>
2025-02-07 15:50:23 -0700

    added player to game via player.py, inheriting from circleshape.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,13 +8,22 @@ def main():
     #print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    frames = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        dt = frames.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
